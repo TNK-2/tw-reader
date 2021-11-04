b62100818b10ff4fda5e76e899c4063c59f47910
Yu-s <yuuu1993g@gmail.com>
2021-11-05 08:03:59 +0900

    add module setting

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
 @NgModule({
-  declarations: [SidebarComponent],
+  declarations: [SidebarComponent, HeaderComponent],
   imports: [
     CommonModule
   ]
