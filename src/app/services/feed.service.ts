37b31ee5a9ca16140023e2a003c746638d1060bf
Yu-s <yuuu1993g@gmail.com>
2021-11-07 11:58:17 +0900

    mock impl heed service

--- src/app/services/feed.service.ts
+++ src/app/services/feed.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators'
+import { Feed } from '../models/feed';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedService {
+  private feedUrl = "api/feeds"
+  httpOptions = {
+    headers: new HttpHeaders({"Content-Type": "application/json"})
+  };
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
+  list(): Observable<Feed[]> {
+    // return this.http.get<Feed[]>(this.feedUrl)
+    //   .pipe(
+    //     tap(feeds => console.log(feeds)),
+    //     catchError(this.handleError<Feed[]>("list all feeds", []))
+    //   )
+    return of([])
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.error(`${operation} 失敗 : ${error.message}`);
+      return of(result as T);
+    };
+  }
 }
